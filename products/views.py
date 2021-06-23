4527f7f97cee1748a72c8fce6f39eaa8058bb406
Aramantos <john.doyle.mail@icloud.com>
2021-06-23 12:21:14 +0000

    add sorting and product counts to product page

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
 
+# Create your views here.
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -19,13 +20,15 @@ def all_products(request):
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
-                products = products.annonate(lower_name=Lower('name'))
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
-                direction == request.GET['direction']
+                direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -36,7 +39,7 @@ def all_products(request):
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
@@ -45,7 +48,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
-        'current_category': categories,
+        'current_categories': categories,
         'current_sorting': current_sorting,
     }
 
