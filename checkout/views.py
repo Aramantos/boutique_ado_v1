66f445262f554ef0bf1653fbc04c4ec80a7221c7
Aramantos <john.doyle.mail@icloud.com>
2021-06-26 17:34:19 +0000

    add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51J6dsDIxAfU6TjYmTZt4Ut4We49olxBAdp5W0uQbQo6k7fctzC3j67U95wW7vBluRF7uQF0wjx1fA4hvExPeG1Ao00oXLKWPzW',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
